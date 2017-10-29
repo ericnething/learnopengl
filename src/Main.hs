49b0c3f303ee729274c60adb2274bee253404cbe
Eric Nething <emnething@gmail.com>
2017-10-29 12:04:32 -0400

    Set texture to repeat on wrap

--- src/Main.hs
+++ src/Main.hs
@@ -182,6 +182,8 @@ initResources game = do
   -- Texture
   texture <- overPtr $ glGenTextures 1
   glBindTexture GL_TEXTURE_2D texture
+  glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_REPEAT
+  glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_REPEAT
   glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR
   glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR
   Image width height texData <- convertRGB8 <$>
